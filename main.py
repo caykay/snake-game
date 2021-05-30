d7826c067a321183a58316278cf5336be4476c3b
caykay <kahemacassian@gmail.com>
2021-05-30 23:31:56 +0930

    Improve snake movements.

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 # STEPS
-# 1. Create a snake body
-# 2. Move the snake
+# 1. Create a snake body  ✅
+# 2. Move the snake  ✅
 # 3. Create snake food
 # 4. Detect collision w/ food
 # 5. Create a scoreboard
@@ -19,11 +19,18 @@ my_screen.title("The Snake Game")
 my_screen.tracer(0)  # turn tracer off / animation off
 
 snake = Snake()
+
+my_screen.listen()
+my_screen.onkey(snake.up, "Up")
+my_screen.onkey(snake.down, "Down")
+my_screen.onkey(snake.left, "Left")
+my_screen.onkey(snake.right, "Right")
+
 while snake.is_alive:
     my_screen.update()  # turn tracer on / update animation after progress happens
+    # we use the timer to delay the animation refresh otherwise we cannot see it happen
     time.sleep(.1)  # slightly delay by .1 seconds after every animation update
-    # if the sleep is not used then the duration window of screen.update() will be
-    # a fraction of a second not visible by the eye
+
     snake.move()
 
 my_screen.exitonclick()
--- snake.py
+++ snake.py
@@ -2,16 +2,22 @@ from turtle import Turtle
 
 STARTING_POS = [(0, 0), (-20, 0), (-40, 0)]  # starting positions
 MOVE_DISTANCE = 20  # distance is 20 because that is the size of each box
+UP = 90
+DOWN = 270
+LEFT = 180
+RIGHT = 0
 
 
-class Snake:
+class Snake:  # 🐍
     """Creation and motion of the snake's body is managed by this class's instance """
     def __init__(self):
         self.fragments = []
         self.is_alive = True
         self.create_body()
+        self.head = self.fragments[0]
 
     def create_body(self):
+        """Create the initial snake body. initial size = 3 fragments."""
         for pos in STARTING_POS:
             new_frag = Turtle("square")
             new_frag.color("white")
@@ -20,10 +26,37 @@ class Snake:
             self.fragments.append(new_frag)
 
     def move(self):
+        """The moving mechanics are such that the last fragment follows the position of that next to it
+        to the first fragment. This way it is easier to keep track of the positioning increment."""
         for frag_num in range(self.length() - 1, 0, -1):
             new_pos = self.fragments[frag_num - 1].pos()
             self.fragments[frag_num].goto(new_pos)
         self.fragments[0].forward(MOVE_DISTANCE)
 
+    def up(self):
+        """Go up.
+        Only go up when not pointing down"""
+        if self.head.heading() != DOWN:
+            self.head.setheading(UP)
+
+    def down(self):
+        """Go south.
+        Only turn south when not facing up"""
+        if self.head.heading() != UP:
+            self.head.setheading(DOWN)
+
+    def left(self):
+        """Turn left.
+        Cannot turn left if facing right"""
+        if self.head.heading() != RIGHT:
+            self.head.setheading(LEFT)
+
+    def right(self):
+        """Turn right.
+        Only turn right when not facing left"""
+        if self.head.heading() != LEFT:
+            self.head.setheading(RIGHT)
+
     def length(self):
+        """Current number of fragments."""
         return len(self.fragments)
